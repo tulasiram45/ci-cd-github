f9bb021d8aaf09d8553223bad54f1e6b5c2f5a6a
deepak1067 <deepak@knoldus.com>
2023-07-04 18:48:33 +0530

    deployment test18

--- src/main/java/com/example/demo2/Controller.java
+++ src/main/java/com/example/demo2/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/hello")
     public String hello_page() {
-        return "hello nashtech!";
+        return "hello nashtech!!!";
     }
 }
