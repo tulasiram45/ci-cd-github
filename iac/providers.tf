06807f0680ebc2730a3c72533129fd72669b5d60
tulasiram45 <43593612+tulasiram45@users.noreply.github.com>
2025-03-12 20:07:12 +0530

    Update providers.tf

--- iac/providers.tf
+++ iac/providers.tf
@@ -2,3 +2,9 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+terraform {
+  backend "gcs" {
+    bucket = "my-project-id-tfstate"
+    prefix = "terraform/state"
+  }
+}
