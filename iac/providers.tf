0b6c4ac361e9a88e56531d7679e95888fc4816b4
tulasiram45 <43593612+tulasiram45@users.noreply.github.com>
2025-03-13 15:29:34 +0530

    Update providers.tf

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0" # Or the version you need
+    }
+  }
+}
 provider "google" {
   project = var.project_id
   region  = var.region
